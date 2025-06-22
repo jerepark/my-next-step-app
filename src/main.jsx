cdfacce45f25909e0cb1afe94a63c193bb899baf
jerepark <jppsychologist@gmail.com>
2025-06-22 21:17:56 +1000

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import GamePlanApp from './GamePlanApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GamePlanApp />
+  </React.StrictMode>
+)
